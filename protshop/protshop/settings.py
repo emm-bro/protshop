6054ff0d47a13db95a6124cc7aaa26b0ded84258
emm-bro <dremmbro@gmail.com>
2015-07-29 12:03:53 +0300

    add settings for postgres: db - djangodb usr - postgres pass - sv

--- protshop/protshop/settings.py
+++ protshop/protshop/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'protshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'USER': 'postgres',
+        'PASSWORD': 'supervis',
     }
 }
 
